c909be273dba7e1c02610bee43da26dd477f5b3b
kikizl <zlcharon94@gmail.com>
2019-09-06 17:29:56 +0800

    Add get-request handling

--- a.js
+++ a.js
@@ -1,13 +1,20 @@
 const http = require('http')
+const queryString = require('querystring')
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'content-type': 'text/html'})
-    res.end('<h1>hello world</h1>')
+    console.log(req.method)
+    const url = req.url
+    req.query = queryString.parse(url.split('?')[1])
+    res.end(
+        JSON.stringify(req.query)
+    )
 })
 
 server.listen(3000, () => {
     console.log('listening on 3000 port')
 })
+
+console.log('ok')
 
 // function add(a, b) {
 //     return a + b
