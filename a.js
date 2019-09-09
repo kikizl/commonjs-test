08ad6577779c06cb1253d0e883049757a455aee9
kikizl <zlcharon94@gmail.com>
2019-09-09 16:48:54 +0800

    Practice post handling

--- a.js
+++ a.js
@@ -1,13 +1,28 @@
 const http = require('http')
-const queryString = require('querystring')
+// const queryString = require('querystring')
 
+// const server = http.createServer((req, res) => {
+//     console.log(req.method)
+//     const url = req.url
+//     req.query = queryString.parse(url.split('?')[1])
+//     res.end(
+//         JSON.stringify(req.query)
+//     )
+// })
 const server = http.createServer((req, res) => {
-    console.log(req.method)
-    const url = req.url
-    req.query = queryString.parse(url.split('?')[1])
-    res.end(
-        JSON.stringify(req.query)
-    )
+    if(req.method == 'POST') {
+        // req 数据格式
+        console.log('req content-type', req.headers['content-type'])
+        // 接收数据
+        let postData = ''
+        req.on('data', chunk => {
+            postData += chunk.toString() // chunk本身是二进制格式
+        })
+        req.on('end', () => {
+            console.log('postData: ', postData)
+            res.end('hello world !') 
+        })
+    }
 })
 
 server.listen(3000, () => {
